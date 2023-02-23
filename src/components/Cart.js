1eb536f3f4e5c5aea42acb18c4d49a8fea7a13eb
pattsunade <patricia.stylesh@gmail.com>
2023-02-23 00:23:35 -0300

    registro y login

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHeart,
   faShoppingCart,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
+import {
+  CModal,
+  CModalBody,
+  CModalHeader,
+  CModalTitle,
+  CModalFooter,
+} from "@coreui/react";
+import {
+  CButton,
+  CFormLabel,
+  CCol,
+  CForm,
+  CRow,
+  CFormInput,
+  CCardImage,
+} from "@coreui/react";
+import Logo from "../assets/img/logoBellaCat.png";
 import "../assets/css/cart.css";
 
 const Car = () => {
+  const [visibleLg, setVisibleLg] = useState(false);
+  const [inicioSeccion, setInicioSeccion] = useState(false);
+ 
   return (
     <div className="botones container text-right">
-        
-      <a href="#" className="btn border primero">
+      <a href="#" onClick={() => setVisibleLg(true)}>
         <FontAwesomeIcon icon={faUser} />
         {/* <span className="badge">0</span> */}
+        <CModal
+          size="lg"
+          visible={visibleLg}
+          onClose={() => setVisibleLg(false)}
+          className="modalContainer"
+        >
+          <CModalHeader>
+            <CModalTitle className="text-center container ">
+              <CCardImage
+                orientation="top"
+                className=" logoRegister"
+                src={Logo}
+              />
+            </CModalTitle>
+          </CModalHeader>
+          <CModalBody className="modalForm">
+            <CForm visible={inicioSeccion}>
+              <CRow className="mb-3">
+                <CFormLabel
+                  htmlFor="inputEmail3"
+                  className="col-sm-2 col-form-label"
+                >
+                  Correo
+                </CFormLabel>
+                <CCol sm={6}>
+                  <CFormInput type="email" id="inputEmail3" className="inputForm"/>
+                </CCol>
+              </CRow>
+              <CRow className="mb-3">
+                <CFormLabel
+                  htmlFor="inputPassword3"
+                  className="col-sm-2 col-form-label "
+                >
+                  Contraseña
+                </CFormLabel>
+                <CCol sm={6}>
+                  <CFormInput type="password" id="inputPassword3" className="inputForm" />
+                </CCol>
+              </CRow>
+
+              <CCol xs={12}>
+           
+            <CButton color="primary" type="submit" className="btnRegister sing">Iniciar Sesion    </CButton>
+    </CCol>
+            </CForm>
+          </CModalBody>
+          <CModalFooter>
+           <a href="#">¿Olvidaste tu contraseña?</a>
+          </CModalFooter>
+
+         
+        </CModal>
       </a>
 
       <a href="#" className="btn border">
--- src/components/Registro.js
+++ src/components/Registro.js
@@ -1,47 +1,162 @@
-import React from "react";
+import React, {useState} from "react";
 import {
-    CButton,
-    CFormLabel,
-    CCol,
-    CForm,
-    CRow,
-    CFormInput,
-    CFormSelect,
-    CFormCheck
-  } from "@coreui/react";
-const Registro = () => {
+  CButton,
+  CFormLabel,
+  CCol,
+  CForm,
+  CRow,
+  CFormInput,
+  CFormSelect,
+  CFormCheck,
+  CCard,
+  CCardImage,
+  CCardBody,
+  CCardTitle,
+  CCloseButton,
+  CCardFooter,
+  CCardText,
+  CCardHeader,
+  CInputGroup,
+  CInputGroupText
+} from "@coreui/react";
+import Header from "./Header";
+import Logo from "../assets/img/logoBellaCat.png";
+import { Link } from 'react-router-dom';
 
-    return (
-        <CForm className="row g-3">
-        <CCol md={6}>
-          <CFormInput type="email" id="inputEmail4" label="Email" />
-        </CCol>
-        <CCol md={6}>
-          <CFormInput type="password" id="inputPassword4" label="Password" />
-        </CCol>
-        <CCol xs={12}>
-          <CFormInput id="inputAddress" label="Address" placeholder="1234 Main St"/>
-        </CCol>
-        <CCol xs={12}>
-          <CFormInput id="inputAddress2" label="Address 2" placeholder="Apartment, studio, or floor"/>
-        </CCol>
-        <CCol md={6}>
-          <CFormInput id="inputCity" label="City"/>
-        </CCol>
-        <CCol md={4}>
-          <CFormSelect id="inputState" label="State">
-            <option>Choose...</option>
-            <option>...</option>
-          </CFormSelect>
-        </CCol>
-        <CCol md={2}>
-          <CFormInput id="inputZip" label="Zip" />
-        </CCol>
-       
-        <CCol xs={12}>
-          <CButton type="submit">Sign in</CButton>
-        </CCol>
-      </CForm>
-    )
-}
+
+//CSS
+import "../assets/css/registro.css";
+import "../assets/css/logo.css";
+
+
+
+
+const Registro = () => {
+ 
+
+  return (
+    <div>
+      <Header />
+      <CCard className="text-center container card col-12-lg">
+        <CCardHeader>
+          <CCardImage orientation="top" className=" logoRegister" src={Logo} />
+        </CCardHeader>
+
+        <CCardBody>
+          <CCardTitle className="title">Registrate </CCardTitle>
+          <CCardText className="subTitle">Rapido y Facil</CCardText>
+          <CForm
+            className="row needs-validation "
+           
+          >
+            <CCol md={6}>
+              <CFormInput
+                type="input"
+                id="validationDefault01"
+                className="form"
+                label="Nombre"
+                
+        
+        required
+              />
+              </CCol>
+            <CCol md={6}>
+              <CFormInput
+                type="input"
+                id="validationDefault02"
+                className="form"
+                label="Apellido"
+                defaultValue=""
+                required
+              />
+            </CCol>
+
+            <CCol md={6} className="ccol">
+              <CFormInput id="validationDefault03" required className="form" label="Correo" />
+            </CCol>
+            <CCol md={6} className="ccol">
+              <CFormInput
+                type="password"
+                id="validationDefault04"
+                className="form"
+                label="Password"
+                required
+              />
+            </CCol>
+            
+            <CCol xs={12}>
+           
+            <CButton color="primary" type="submit" className="btnRegister">
+      Registrarse
+    </CButton>
+              
+            </CCol>
+            <CCol xs={12}>
+              <Link to='/'><a href="#" className="back">
+                Volver
+              </a></Link>
+              
+            </CCol>
+          </CForm>
+          {/* <CForm className="row g-3">
+          <CCol md={6}>
+              <CFormInput
+                type="input"
+                id="validationDefault01"
+                className="form"
+                label="Nombre"
+                
+        
+        required
+              />
+ 
+  </CCol>
+  <CCol md={4}>
+    <CFormInput
+      type="text"
+      id="validationDefault02"
+      label="Last name"
+      defaultValue="Otto"
+      required
+    />
+  </CCol>
+  <CCol md={4}>
+    <CFormLabel htmlFor="validationDefaultUsername">Username</CFormLabel>
+    <CInputGroup>
+      <CInputGroupText id="inputGroupPrepend02">@</CInputGroupText>
+      <CFormInput
+        type="text"
+        id="validationDefaultUsername"
+        defaultValue=""
+        aria-describedby="inputGroupPrepend02"
+        required
+      />
+    </CInputGroup>
+  </CCol>
+  <CCol md={6}>
+    <CFormInput type="text" id="validationDefault03" label="City" required />
+  </CCol>
+  <CCol md={3}>
+    <CFormSelect id="validationDefault04" label="State">
+      <option disabled>Choose...</option>
+      <option>...</option>
+    </CFormSelect>
+  </CCol>
+  <CCol md={3}>
+    <CFormInput type="text" id="validationDefault05" label="Zip" required />
+  </CCol>
+  <CCol xs={12}>
+    <CFormCheck type="checkbox" id="invalidCheck" label="Agree to terms and conditions" required />
+  </CCol>
+  <CCol xs={12}>
+    <CButton color="primary" type="submit">
+      Submit form
+    </CButton>
+  </CCol>
+</CForm> */}
+        </CCardBody>
+      </CCard>
+    </div>
+  );
+};
 export default Registro;
